a68ef2278b9ada2da7f445ab652fe8f50a23a8ba
Tássyla Lima <56099401+tatitassyla@users.noreply.github.com>
2021-11-05 08:33:39 -0300

    Initial commit

---
+++ src/main/java/math/Numbers.java
@@ -0,0 +1,14 @@
+package math;
+
+public class Numbers {
+
+    public Boolean quadradoPerfeito(int n){
+        return Math.sqrt(n) % 1 == 0;
+    }
+
+    public int areaRetangulo(int l, int a){
+        return l * a;
+    }
+
+
+}
